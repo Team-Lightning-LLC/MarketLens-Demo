f26ccce33d6825337f93464d7c4d5e9f3e155b1a
Team-Lightning-LLC <lehmanachris@gmail.com>
2025-12-01 14:11:16 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -39,7 +39,7 @@ const CONFIG = {
     ],
     
     "Scenario Modeling": [
-      "Risk Correlation Study",
+      "Risk Modeling",
       "Industry Consolidation: M&A",
       "Monetary Policy & Interest Rate"
     ],
